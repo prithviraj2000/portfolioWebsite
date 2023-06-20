0bfd24e6e42aabc85efd29be3e9f00e598b5c0ce
Prithvi <73948749+prithviraj2000@users.noreply.github.com>
2023-06-20 13:35:17 +0530

    changes done

---
+++ script.js
@@ -0,0 +1,15 @@
+// Typing animation//
+
+var typed=new Typed(".typing",{
+    strings:["Student","Developer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+})
+
+var typed=new Typed(".typing-2",{
+    strings:["Student","Developer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
+})
